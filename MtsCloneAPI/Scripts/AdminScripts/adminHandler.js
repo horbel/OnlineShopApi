8c9325e46e0a097d53d5ecd94beba94ee8291db9
horbel <gorbel.alex@ya.ru>
2016-05-07 15:59:23 +0300

    Edit Brand form has been added

--- MtsCloneAPI/Scripts/AdminScripts/adminHandler.js
+++ MtsCloneAPI/Scripts/AdminScripts/adminHandler.js
@@ -15,6 +15,7 @@
     });
     //EDIT CLICK BTN EVENT!!!!!!!
 
+
 });
 
 //GET FUNCTIONS
@@ -74,21 +75,21 @@ function ShowAllProducts(products) {
             '</tr>';
     }
     result += '</tbody></table>';
-    $('#right-block').html(result);
+    $('#table-block').html(result);
 }
 function ShowAllBrands(brands) {
     var result = '<h2>Таблица брендов</h2> <table id="product-table"><thead>' +
         '<tr><th>№</th><th>Название</th></tr>' +
         '</thead><tbody>';
-    for (var i = 0; i < brands.length; i++) {
+    for (var i = 0; i < brands.length; i++) {        
         result += '<tr><td>' + (i + 1) + '</td>' +     
             '<td>' + brands[i].Name + '</td>' +           
-            '<td class="button-conteiner"><button id="edit-btn">Edit</button></td>' +
+            '<td class="button-conteiner"><button data-item="'+brands[i].ID+'" onclick="EditItem(this)" id="edit-brand-form-btn">Edit</button></td>' +
             '<td class="button-conteiner"><button id="delete-btn">Delete</button></td>' +
             '</tr>';
     }
     result += '</tbody></table>';
-    $('#right-block').html(result);
+    $('#table-block').html(result);
 }
 
 function ShowAllTypes(types) {
@@ -103,17 +104,43 @@ function ShowAllTypes(types) {
             '</tr>';
     }
     result += '</tbody></table>';
-    $('#right-block').html(result);
+    $('#table-block').html(result);
 }
 
-function ShowBrand(book) {
-    $('#right-block').html('');
+function ShowBrand(brand) {
+    $('#table-block').css('display', 'none');
     $('#edit-brand-block').css('display', 'block');
+    $('#edit-brand-id').val(brand.ID);
+    $('#edit-brand-name').val(brand.Name);
+    $('#edit-brand-button').click(function (event) {
+        event.preventDefault();
+        SaveBrandChange(brand);
+    })
+}
+function SaveBrandChange(brand) {
+    var newBrand = {
+        ID: $('#edit-brand-id').val(),
+        Name: $('#edit-brand-name').val()
+    }
+    $.ajax({
+        url: '/api/brand/' + newBrand.ID,
+        type: 'PUT',
+        data: JSON.stringify(newBrand),
+        contentType: "application/json;charset=utf-8",
+        success: function (data) {
+            $('#edit-brand-block').css('display', 'none');
+            $('#table-block').css('display', 'block');
+            GetAllBrands(function (brands) { ShowAllBrands(brands) })
+        },
+        error: function (x, y, z) {
+            alert(x + '\n' + y + '\n' + z);
+        }
+    })
 }
 //EDIT FUNCTIONS
 function GetBrand(id) {
     $.ajax({
-        url: '/api/brand' + id,
+        url: '/api/brand/' + id,
         type: 'GET',
         dataType: 'json',
         success:function(data){
@@ -123,4 +150,10 @@ function GetBrand(id) {
             alert(x + '/n' + y + '/n' + z + '/n');
         }            
     })
+}
+
+function EditItem(el) {
+    // получаем id редактируемого объекта
+    var id = $(el).attr('data-item');
+    GetBrand(id);
 }
